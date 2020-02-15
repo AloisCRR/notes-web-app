9a3ec5f063ff8cfc1f9def82a7e1b02c2e8a79d1
Alois <aloiscarrera@gmail.com>
2020-02-15 15:41:48 -0500

    Individual user schema for db

---
+++ src/models/user.js
@@ -0,0 +1,31 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {
+    timestamps: true
+})
+
+userSchema.methods.cryptPass = async pass => {
+    const salt = await bcrypt.genSalt();
+    return await bcrypt.hash(pass, salt);
+}
+
+userSchema.methods.matchPass = function (pass) {
+    return await bcrypt.compare(pass, this.password);
+}
+
+module.exports = model('user', userSchema);
