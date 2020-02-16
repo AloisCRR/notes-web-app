b8edfc8e4f5c47a0e74c4eab6af3cf7498d222d4
Alois <aloiscarrera@gmail.com>
2020-02-16 15:04:16 -0500

    Update functionality

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -1,31 +1,34 @@
 const ntsCtrl = {};
 const Note = require('../models/note');
 
-ntsCtrl.renderNoteForm = (req,res)=>{
-    res.render('notes/add-page');
+ntsCtrl.renderNoteForm = (req, res) => {
+    res.render('notes/add-note');
 };
 
-ntsCtrl.renderAddNote = async (req,res)=>{
+ntsCtrl.renderAddNote = async (req, res) => {
     const { noteDescription, noteTitle } = req.body;
-    const newNote = new Note({title: noteTitle, description: noteDescription});
+    const newNote = new Note({ title: noteTitle, description: noteDescription });
     await newNote.save();
     res.redirect('/notes');
 };
 
-ntsCtrl.renderAllNotes = async (req, res)=>{
+ntsCtrl.renderAllNotes = async (req, res) => {
     const note = await Note.find();
     res.render('notes/all-notes', { note });
 }
 
-ntsCtrl.renderEditNote = (req,res)=>{
-    res.send(req.params.id)
+ntsCtrl.renderEditNote = async (req, res) => {
+    const note = await Note.findById(req.params.id);
+    res.render('notes/edit-note', { note })
 };
 
-ntsCtrl.renderUpdateNote = (req, res)=>{
-    res.send('updated')
+ntsCtrl.renderUpdateNote = async (req, res) => {
+    const { noteTitle, noteDescription } = req.body;
+    await Note.findByIdAndUpdate(req.params.id, { title: noteTitle, description: noteDescription})
+    res.redirect('/notes');
 }
 
-ntsCtrl.renderDeleteNote = async (req, res) =>{
+ntsCtrl.renderDeleteNote = async (req, res) => {
     await Note.findByIdAndDelete(req.params.id);
     res.redirect('back');
 }
