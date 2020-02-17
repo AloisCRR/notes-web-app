340c100a09998081c485e00fba9efed42cd02951
Alois <aloiscarrera@gmail.com>
2020-02-16 22:09:50 -0500

    Signup function 😊

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,11 +1,35 @@
 const usrCtrl = {};
 
+const User = require('../models/user');
+
 usrCtrl.renderSignUp = (req, res) => {
     res.render('users/signup');
 }
 
-usrCtrl.signUp = (req, res) => {
-    res.send('Signed up');
+usrCtrl.signUp = async (req, res) => {
+    const { name, email, password, conf_password } = req.body;
+    const errors = [];
+    if (password != conf_password) {
+        errors.push({ text: 'Passwords does not match' });
+    }
+    if (password.length <= 5) {
+        errors.push({ text: 'Password too weak, it must be at least 6 characters' });
+    }
+    if (errors.length > 0) {
+        res.render('users/signup', { errors, name, email });
+    } else {
+       const userEmail = await User.findOne({email});
+       if (userEmail) {
+           req.flash('err_msg', 'This mail was already registered.')
+           res.redirect('back');
+       } else {
+           const newUser = new User({name,email,password});
+           newUser.password = await newUser.cryptPass(password);
+           await newUser.save();
+           req.flash('success_msg', 'Signup complete!')
+           res.redirect('/notes');
+       }
+    }
 }
 
 usrCtrl.renderlogIn = (req, res) => {
